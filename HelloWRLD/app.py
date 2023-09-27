2d0a2175b524539ac0e410f6e09cbb9f3939720a
Abenathi201 <abenathi.sindaphi201@gmail.com>
2023-09-27 14:08:22 +0200

    first commit

---
+++ HelloWRLD/app.py
@@ -0,0 +1,14 @@
+# Printing name and favourite coloue
+# name = input("What is your name? ")
+# colour = input("What is your favourite colour? ")
+# print(name + " likes " + colour)
+
+# Calculating your year
+# birth_year = input("What is your birth year: ")
+# age = 2023 - int(birth_year)
+# print(age)
+
+# Converting pounds to kilograms
+# pounds = input("What is your weight in pounds? ")
+# kilograms = int(pounds) * 0.454
+# print(kilograms)
